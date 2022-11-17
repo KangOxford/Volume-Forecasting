4b25bee5c77fae39e3809d3b14769b698b2705d3
KangOxford <kang.li@maths.ox.ac.uk>
2022-11-17 16:23:33 +0000

    Update data_pipeline.py

--- data_processing/data_pipeline.py
+++ data_processing/data_pipeline.py
@@ -9,6 +9,7 @@ def get_message_data():
     df = pd.read_csv(data_path)
     message = df[df.iloc[:,1] == 4];message.columns = ['time','type','order_id','quantity','price','side','remark']
     message = message.drop(['remark'],axis = 1)
+    message = message.reset_index()
     return message
 
 def get_orderbook_data():
@@ -16,16 +17,16 @@ def get_orderbook_data():
     data_path = "/Users/kang/Desktop/Volume-Tranformer/AMZN_2021-04-01_34200000_57600000_orderbook_50.csv"
     df = pd.read_csv(data_path)
     df1 = df.iloc[:,[0,2]];df1.columns =['best_ask','best_bid']
+    df1['mid_price'] = (df1.best_ask + df1.best_bid)//2
+    df1 = df1.reset_index()
     return df1
-    # df2 = pd.merge(message, df1,left_index=True, right_index=True);df2 = df2.reset_index();df2 = df2.drop(['remark'],axis = 1);df2
-    # pass
 
 def timestamp_format(message):
     message.time *= 1000000000
     message.time = message.time.astype("datetime64[ns]")
     message = message.reset_index()
     message = message.set_index('time')
-    message = message.drop(['index'],axis = 1)
+    message = message.drop(['index','level_0'],axis = 1)
     return message
     
 def plot_single_value(value):
@@ -47,11 +48,18 @@ def cut_tail(groupped_quantity):
     return new 
 
 def get_basic_features(groupped_message):
+    def get_num_vol_ntn(item, signal, sym = ''):
+        bid_item = item[item.side == 1]; ask_item = item[item.side == -1]
+        signal[sym + 'bid_num_orders'] = bid_item.shape[0]; signal[sym+'ask_num_orders'] = ask_item.shape[0]
+        signal[sym+'bid_volume'] = bid_item.quantity.sum(); signal[sym+'ask_volume'] = ask_item.quantity.sum()        
+        signal[sym+'bid_notianal']=(bid_item.quantity * bid_item.price).sum()/Config.scale_level
+        signal[sym+'ask_notianal']=(ask_item.quantity * ask_item.price).sum()/Config.scale_level
+        return signal
     signal_list = []
     for time_index, item in groupped_message:
         # ----------------- 01 -----------------
         signal = {}
-        signal['timeHM_start'] = str(time_index[0]).zfill(2) + str(time_index[1])
+        signal['timeHM_start'] = str(time_index[0]).zfill(2) + str(time_index[1]).zfill(2)
         x_bid = sum(item.side == 1); x_ask = sum(item.side == -1)
         signal['imbalance'] = (x_bid - x_ask)/(x_bid + x_ask)
         
@@ -62,12 +70,14 @@ def get_basic_features(groupped_message):
         else:
             signal['intrady_session'] = 1
         # ----------------- 02 -----------------
-        bid_item = item[item.side == 1]; ask_item = item[item.side == -1]
-        signal['bid_num_orders'] = bid_item.shape[0]; signal['ask_num_orders'] = ask_item.shape[0]
-        signal['bid_volume'] = bid_item.quantity.sum(); signal['ask_volume'] = ask_item.quantity.sum()        
-        signal['bid_notianal']=(bid_item.quantity * bid_item.price).sum()/Config.scale_level
-        signal['ask_notianal']=(ask_item.quantity * ask_item.price).sum()/Config.scale_level
+        signal = get_num_vol_ntn(item, signal)
         # ----------------- 03 -----------------
+        item['aggressive'] = ((item.price >= item.mid_price) & (item.side == 1)) | ((item.price <= item.mid_price) & (item.side == -1))
+        aggressive = item[item.aggressive]
+        signal = get_num_vol_ntn(aggressive, signal, sym = 'ag_')
+        # # ----------------- 04 -----------------
+        # signal['volume'] = item.quantity.sum()
+        # ----------------- 05 -----------------
         signal_list.append(signal)
         # print() #$
     features = pd.DataFrame(signal_list)
@@ -76,8 +86,11 @@ def get_basic_features(groupped_message):
 
 if __name__=="__main__":
     message = get_message_data()
-    message = timestamp_format(message)
-    groupped_message, groupped_quantity = split_into_bucket(message)
+
+    orderbook_data = get_orderbook_data()
+    merged_message = pd.merge(message, orderbook_data, how = "left")
+    merged_message = timestamp_format(merged_message)
+    groupped_message, groupped_quantity = split_into_bucket(merged_message)
     # plot_single_value(groupped_quantity.values)
     features = get_basic_features(groupped_message)
     
