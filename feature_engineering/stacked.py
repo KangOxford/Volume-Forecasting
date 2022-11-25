dc63b10a37ba236eecc21a4c082b1ad76152671c
KangOxford <kang.li@maths.ox.ac.uk>
2022-11-25 15:23:22 +0000

    Update stacked.py

--- feature_engineering/stacked.py
+++ feature_engineering/stacked.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from os import listdir; from os.path import isfile, join
-mypath = "/Users/kang/Desktop/Volume-Forecasting/data_processing/data/"
+path = "/Users/kang/Desktop/Volume-Forecasting/data_processing/data/"
+subpath = 'disjoint1_5'
+mypath = path + subpath +'/'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 filename_list = [f[:-4] for f in onlyfiles]
 single_1_list = []
@@ -11,10 +13,5 @@ disjoint1_5_list = []
 disjoint1_5_10_list = []
 for i in range(len(onlyfiles)):
     name = filename_list[i]
-    # print(name)
-    # print(name[7:])
-    # print("-----")
     file = pd.read_csv(mypath+onlyfiles[i])
     exec('date_'+ name + " = file")
-    if name[7:] == "single_1":
-        single_1_list.append()
