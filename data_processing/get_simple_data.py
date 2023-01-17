f5a1c3dc983586fb7ba1edf81b21a5caffc63771
KangOxford <kang.li@maths.ox.ac.uk>
2023-01-17 21:54:31 +0800

    Update get_simple_data.py

--- data_processing/get_simple_data.py
+++ data_processing/get_simple_data.py
@@ -35,8 +35,6 @@ def get_basic_features(groupped_message, window_size=1):
     return features
 
 
-
-
 def get_simple_features(groupped_message, window_size=1):
     w = window(groupped_message, window_size)
     signal_list = []
@@ -62,18 +60,39 @@ def get_simple_features(groupped_message, window_size=1):
     return features
 
 
-#
-# if __name__ == "__main__":
-#     pass
-index, symbol = 0, "210426"
-window_size = 1
-message = get_message_data(index)
-orderbook_data = get_orderbook_data(index)
+def get_volume(groupped_message, window_size=1):
+    w = window(groupped_message, window_size)
+    signal_list = []
+    for next_w in w:
+        # ----------------- 01 -----------------
+        list_ = [item[1] for item in next_w]
+        item = pd.concat(list_)
+        time_index_start = next_w[0][0]
+        # ----------------- 01 -----------------
+        signal = {}
+        time_index_start = time_index_formatting(time_index_start)
+        signal['timeHM_start'] = str(time_index_start[0]).zfill(2) + str(time_index_start[1]).zfill(2)
+        # ----------------- 02 -----------------
+        signal = get_num_vol_ntn(item, signal)
+        # # --------------- 04 -----------------
+        signal['volume'] = item.quantity.sum()
+        # ----------------- 05 -----------------
+        signal_list.append(signal)
+        # print() #$
+    features = pd.DataFrame(signal_list)
+    return features
 
-merged_message = pd.merge(message, orderbook_data, how="left")
-merged_message = timestamp_format(merged_message)
-groupped_message = split_into_bucket(merged_message)
-# plot_single_value(groupped_quantity.values)
-features = get_basic_features(groupped_message, window_size)
+if __name__ == "__main__":
+    pass
+    index, symbol = 0, "210426"
+    window_size = 1
+    message = get_message_data(index)
+    orderbook_data = get_orderbook_data(index)
+
+    merged_message = pd.merge(message, orderbook_data, how="left")
+    merged_message = timestamp_format(merged_message)
+    groupped_message = split_into_bucket(merged_message)
+    # plot_single_value(groupped_quantity.values)
+    features = get_simple_features(groupped_message, window_size)
 
 
