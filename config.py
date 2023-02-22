1f8becaceb0311468b3ce4ca18a7443523ec3567
KangOxford <kang.li@maths.ox.ac.uk>
2023-02-23 01:35:21 +0800

    init

--- config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Config:
-    def __init__(self):
-        
-    day_list = ['01', '05', '06', '07',]
--- feature_engineering/utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def data_split(data,size = 10):
-    X = data.iloc[:-size,:-1]
-    Y = data.iloc[:-size,-1]
-    pred_x = data.iloc[-size:,:-1]
-    real_y = data.iloc[-size:,-1]  
-    return (X,Y),(pred_x,real_y)
