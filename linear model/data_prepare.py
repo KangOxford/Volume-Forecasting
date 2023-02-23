ce155c20da82d2bb1a35916845c2b0085b890a03
KangOxford <kang.li@maths.ox.ac.uk>
2023-02-23 15:21:41 +0800

    update

--- linear model/data_prepare.py
+++ linear model/data_prepare.py
@@ -5,15 +5,15 @@ data_path = "/Users/kang/Data/2017/"
 date = "20170710"
 sym = "AAPL"
 
-df = pd.read_csv(data_path+date+'/'+date + '-'+ sym+'.csv').iloc[:,3:].drop(['volBuyQty_lit',
-       'volBuyNotional_lit', 'volBuyNrTrades_lit', 'volSellQty_lit',
-       'volSellNotional_lit', 'volSellNrTrades_lit', 'volBuyQty_hid',
-       'volBuyNotional_hid', 'volBuyNrTrades_hid', 'volSellQty_hid',
-       'volSellNotional_hid', 'volSellNrTrades_hid', 'jump_value', 'is_jump',
-       'signed_jump'],axis =1)
 df = pd.read_csv(data_path+date+'/'+date + '-'+ sym+'.csv')[['timeIndex', 'timeHMs', 'timeHMe', 'volBuyQty', 'volBuyNotional',
        'volSellQty', 'volSellNotional', 'nrTrades', 'bidPx', 'askPx', 'bidQty',
        'askQty', 'pret_1m', 'symbol', 'vol']]
+
+from os import listdir
+mypath = data_path
+onlyfiles = [f for f in listdir(mypath)]
+pd.DataFrame(onlyfiles).to_csv(data_path+'trading_days2017.csv')
+
 df.date = pd.to_datetime(df.date)
 
 gpd = df.set_index('date').groupby(pd.Grouper(freq='D'))
