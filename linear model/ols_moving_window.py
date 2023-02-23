4d67f5ac73647f76bc5b18157ceb74c79a1fbf90
KangOxford <kang.li@maths.ox.ac.uk>
2023-02-24 00:55:11 +0800

    update

--- linear model/ols_moving_window.py
+++ linear model/ols_moving_window.py
@@ -3,7 +3,8 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from os import listdir;from os.path import isfile, join
-import statsmodels.api as sm
+import statsmodels.api as s
+from sklearn.metrics import mean_squared_error as mse
 
 '''1. transfer data format from format A to B'''
 '''2. fulfill nan with the mean of surrounding values'''
@@ -64,17 +65,21 @@ X0 = dflst.iloc[:,1:-1]
 X0 = X0[['date',"intrSn","qty","volBuyQty","volSellQty","volBuyNotional","nrTrades"]]
 y0 = dflst.iloc[:,-1]
 
-index = 10000
-
-
 window_size = 3900
 rst_lst = []
-for index in range(window_size,X0.shape[0]):
+for index in range(window_size, X0.shape[0]):
+    print(index)
     X = X0.iloc[index-window_size:index,:]
     y = y0.iloc[index-window_size:index]
     X = sm.add_constant(X, has_constant='add')
     results = sm.OLS(y, X).fit()
-    print(results.summary())
     y_hat = results.predict([1] +list(X0.iloc[index,:]))
     y_true = y0.iloc[index]
-    rst_lst.append((y_true, y_hat))
+    rst_lst.append([y_true, y_hat])
+rst = pd.DataFrame(rst_lst)
+rst.iloc[:,-1] = rst.iloc[:,-1].apply(lambda x:x[0])
+
+# plt.plot(rst.iloc[:,0])
+# plt.plot(rst.iloc[:,1])
+# plt.show()
+# mse(rst.iloc[:,0], rst.iloc[:,1])
