6415bdaf9e9645569c22d78a24ae891af799f719
KangOxford <kang.li@maths.ox.ac.uk>
2023-02-23 18:51:50 +0800

    update

---
+++ linear model/before_ols.py
@@ -0,0 +1,43 @@
+df.date = pd.to_datetime(df.date)
+gpd = df.set_index('date').groupby(pd.Grouper(freq='D'))
+x_list, y_list = [], []
+for index, data in gpd:
+    x = data.iloc[:-1,-1]
+    y = data.iloc[1:,-1]
+    x_list.append(x); y_list.append(y)
+x = pd.concat(x_list); y = pd.concat(y_list)
+
+
+
+
+
+
+def data_split(data,size = 10):
+    X = data.iloc[:-size,:-1]
+    Y = data.iloc[:-size,-1]
+    pred_x = data.iloc[-size:,:-1]
+    real_y = data.iloc[-size:,-1]
+    return (X,Y),(pred_x,real_y)
+
+def ols(train, test):
+    def out_of_sample(results, test):
+        pred_x = test[0]
+        pred_x = sm.add_constant(pred_x, has_constant='add')
+        pred_y = results.predict(pred_x)
+        real_y = test[1]
+        from sklearn.metrics import r2_score
+        r_squared = r2_score(real_y, pred_y)
+        return r_squared
+
+    import statsmodels.api as sm
+    X = train[0]
+    # X = sm.add_constant(X)
+    X = sm.add_constant(X, has_constant='add')
+    Y = train[1]
+    results = sm.OLS(Y, X).fit()
+    print(results.summary())
+    return out_of_sample(results, test)
+
+    train, test = data_split(data, size=10)
+    out_of_sample = ols(train, test)
+    print(f">>> out_of_sample: {out_of_sample}")
--- linear model/data_prepare.py
+++ linear model/data_prepare.py
@@ -2,13 +2,36 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
+'''1. transfer data format from format A to B'''
+'''2. fulfill nan with the mean of surrounding values'''
+'''3. generate sym.csv with all the dates merged in single csv'''
+
+'''format A
+['Unnamed: 0', 'Unnamed: 0.1', 'Unnamed: 0.1.1', 'timeIndex', 'timeHMs',
+       'timeHMe', 'volBuyQty', 'volBuyNotional', 'volSellQty',
+       'volSellNotional', 'nrTrades', 'volBuyQty_lit', 'volBuyNotional_lit',
+       'volBuyNrTrades_lit', 'volSellQty_lit', 'volSellNotional_lit',
+       'volSellNrTrades_lit', 'volBuyQty_hid', 'volBuyNotional_hid',
+       'volBuyNrTrades_hid', 'volSellQty_hid', 'volSellNotional_hid',
+       'volSellNrTrades_hid', 'bidPx', 'askPx', 'bidQty', 'askQty', 'pret_1m',
+       'symbol', 'vol', 'jump_value', 'is_jump', 'signed_jump']
+'''
+'''format B
+['symbol', 'date', 'timeHMs', 'timeHMe', 'qty', 'volBuyQty',
+       'volSellQty', 'ntn', 'volBuyNotional', 'volSellNotional', 'nrTrades',
+       'ntr', 'volBuyNrTrades_lit', 'volSellNrTrades_lit']
+'''
+
 path = "/Users/kang/Data/"
 data_path = path + "2017/"
 out_path = path + 'out/'
 trading_dates = pd.read_csv(path+"trading_days2017.csv",index_col=0)['0'].apply(str)
 removed_dates = pd.read_csv(path+"removed_days2017.csv",index_col=0)['0'].apply(str)
 dates = pd.DataFrame({'date':list(set(trading_dates.values).difference(set(removed_dates.values)))}).sort_values('date').reset_index().drop('index',axis=1)['date'].apply(str)
-syms = pd.read_csv(path+"symbols.csv",index_col=0)['0'].apply(str)
+trading_syms = pd.read_csv(path+"symbols.csv",index_col=0)['0'].apply(str)
+removed_syms = pd.read_csv(path+"removed_syms.csv",index_col=0)['0'].apply(str)
+syms = pd.DataFrame({'syms':list(set(trading_syms.values).difference(set(removed_syms.values)))}).sort_values('syms').reset_index().drop('index',axis=1)['syms'].apply(str)
+
 for i in tqdm(range(len(syms))):
     sym = syms.iloc[i]
     print(f">>> stock {i} {sym}")
@@ -18,16 +41,28 @@ for i in tqdm(range(len(syms))):
 
         df = pd.read_csv(data_path+date+'/'+date + '-'+ sym+'.csv')
         df['qty']=df.volBuyQty+df.volSellQty;df['ntn']= df.volSellNotional+df.volBuyNotional;df['ntr']=df.volBuyNrTrades_lit+df.volSellNrTrades_lit;df['date'] = date
-        df = df[['symbol', 'date', 'timeHMs', 'timeHMe', 'qty', 'volBuyQty','volSellQty', 'ntn', 'volBuyNotional', 'volSellNotional',  'nrTrades','ntr', 'volBuyNrTrades_lit', 'volSellNrTrades_lit']]
+        df['intrSn'] = df.timeHMs.apply(lambda x: 0 if x< 1000 else( 2 if x>=1530 else 1))
+        df = df[['symbol', 'date', 'timeHMs', 'timeHMe', 'intrSn', 'qty', 'volBuyQty','volSellQty', 'ntn', 'volBuyNotional', 'volSellNotional',  'nrTrades','ntr', 'volBuyNrTrades_lit', 'volSellNrTrades_lit']]
 
         def resilient_window_mean_nan(sr):
-            # fullfill with the surrounding 4 non-nan values
-            s_ffill = sr.ffill().ffill()
-            s_bfill = sr.bfill().bfill()
-            s_filled = (s_ffill + s_bfill) / 2
-            return s_filled
+            def double_fullfill(sr):
+                # fullfill with the surrounding 4 non-nan values
+                s_ffill = sr.ffill().ffill()
+                s_bfill = sr.bfill().bfill()
+                s_filled = (s_ffill + s_bfill) / 2
+                return s_filled
+            def ffill(sr):
+                s_ffill = sr.ffill()
+                return s_ffill
+            def bfill(sr):
+                s_bfill = sr.bfill()
+                return s_bfill
+            rst = double_fullfill(sr)
+            rst = ffill(rst)
+            rst = bfill(rst)
+            return rst
 
-        df.iloc[:,4:] = df.iloc[:,4:].apply(resilient_window_mean_nan, axis = 0)
+        df.iloc[:,5:] = df.iloc[:,5:].apply(resilient_window_mean_nan, axis = 0)
         df_list.append(df)
     dflst = pd.concat(df_list)
     dflst.to_csv(out_path+sym+'.csv')
@@ -42,46 +77,4 @@ for i in tqdm(range(len(syms))):
 
 
 
-df.date = pd.to_datetime(df.date)
-gpd = df.set_index('date').groupby(pd.Grouper(freq='D'))
-x_list, y_list = [], []
-for index, data in gpd:
-    x = data.iloc[:-1,-1]
-    y = data.iloc[1:,-1]
-    x_list.append(x); y_list.append(y)
-x = pd.concat(x_list); y = pd.concat(y_list)
 
-
-
-
-
-
-def data_split(data,size = 10):
-    X = data.iloc[:-size,:-1]
-    Y = data.iloc[:-size,-1]
-    pred_x = data.iloc[-size:,:-1]
-    real_y = data.iloc[-size:,-1]
-    return (X,Y),(pred_x,real_y)
-
-def ols(train, test):
-    def out_of_sample(results, test):
-        pred_x = test[0]
-        pred_x = sm.add_constant(pred_x, has_constant='add')
-        pred_y = results.predict(pred_x)
-        real_y = test[1]
-        from sklearn.metrics import r2_score
-        r_squared = r2_score(real_y, pred_y)
-        return r_squared
-
-    import statsmodels.api as sm
-    X = train[0]
-    # X = sm.add_constant(X)
-    X = sm.add_constant(X, has_constant='add')
-    Y = train[1]
-    results = sm.OLS(Y, X).fit()
-    print(results.summary())
-    return out_of_sample(results, test)
-
-    train, test = data_split(data, size=10)
-    out_of_sample = ols(train, test)
-    print(f">>> out_of_sample: {out_of_sample}")
