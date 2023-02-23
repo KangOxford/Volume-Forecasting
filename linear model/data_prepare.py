898b0669b7e5f8779cd04ee9084926b126b9407b
KangOxford <kang.li@maths.ox.ac.uk>
2023-02-23 17:04:57 +0800

    update figure

--- linear model/data_prepare.py
+++ linear model/data_prepare.py
@@ -3,15 +3,34 @@ import numpy as np
 
 path = "/Users/kang/Data/"
 data_path = path + "2017/"
-dates = pd.read_csv(path+"trading_days2017.csv",index_col=0)
-syms = pd.read_csv(path+"symbols.csv",index_col=0)['0'].apply(lambda x:x[9:-4]).to_csv(path+"symbols.csv")
-date = dates[0]
-sym = "AAPL"
+trading_dates = pd.read_csv(path+"trading_days2017.csv",index_col=0)['0'].apply(str)
+removed_dates = pd.read_csv(path+"removed_days2017.csv",index_col=0)['0'].apply(str)
+dates = pd.DataFrame({'date':list(set(trading_dates.values).difference(set(removed_dates.values)))}).sort_values('date').reset_index().drop('index',axis=1)['date'].apply(str)
+syms = pd.read_csv(path+"symbols.csv",index_col=0)['0'].apply(str)
+i,j = 1,2
+date = dates.iloc[i]
+sym = syms.iloc[j]
 
-df = pd.read_csv(data_path+date+'/'+date + '-'+ sym+'.csv')[['timeIndex', 'timeHMs', 'timeHMe', 'volBuyQty', 'volBuyNotional',
-       'volSellQty', 'volSellNotional', 'nrTrades', 'bidPx', 'askPx', 'bidQty',
-       'askQty', 'pret_1m', 'symbol', 'vol']]
+df = pd.read_csv(data_path+date+'/'+date + '-'+ sym+'.csv')
+df['qty']=df.volBuyQty+df.volSellQty;df['ntn']= df.volSellNotional+df.volBuyNotional;df['ntr']=df.volBuyNrTrades_lit+df.volSellNrTrades_lit;df['date'] = date
+df = df[['symbol', 'date', 'timeHMs', 'timeHMe', 'qty', 'volBuyQty','volSellQty', 'ntn', 'volBuyNotional', 'volSellNotional',  'nrTrades','ntr', 'volBuyNrTrades_lit', 'volSellNrTrades_lit']]
 
+def resilient_window_mean_nan(sr):
+    # fullfill with the surrounding 4 non-nan values
+    s_ffill = sr.ffill().ffill()
+    s_bfill = sr.bfill().bfill()
+    s_filled = (s_ffill + s_bfill) / 2
+    return s_filled
+
+df.apply(resilient_window_mean_nan, axis = 1)
+f = resilient_window_mean_nan(df.qty)
+import matplotlib.pyplot as plt
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 300
+plt.plot(f.values)
+plt.plot(df.qty.values)
+# plt.savefig('qty_fullfill.png')
+plt.show()
 
 df.date = pd.to_datetime(df.date)
 
