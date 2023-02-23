c08e9016e9f661768abaaddd37ee5fd1646b340e
KangOxford <kang.li@maths.ox.ac.uk>
2023-02-23 15:27:00 +0800

    update

--- linear model/data_prepare.py
+++ linear model/data_prepare.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
 
-data_path = "/Users/kang/Data/2017/"
-date = "20170710"
+path = "/Users/kang/Data/"
+data_path = path + "2017/"
+dates = pd.read_csv(path+"trading_days2017.csv",index_col=0).sort_values("0")
+dates.reset_index().iloc[1:,1].to_csv(path+"trading_days2017.csv")
 sym = "AAPL"
 
 df = pd.read_csv(data_path+date+'/'+date + '-'+ sym+'.csv')[['timeIndex', 'timeHMs', 'timeHMe', 'volBuyQty', 'volBuyNotional',
        'volSellQty', 'volSellNotional', 'nrTrades', 'bidPx', 'askPx', 'bidQty',
        'askQty', 'pret_1m', 'symbol', 'vol']]
 
-from os import listdir
-mypath = data_path
-onlyfiles = [f for f in listdir(mypath)]
-pd.DataFrame(onlyfiles).to_csv(data_path+'trading_days2017.csv')
 
 df.date = pd.to_datetime(df.date)
 
