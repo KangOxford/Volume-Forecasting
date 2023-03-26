d2431f694c5e460a4f42818a23fc6a68fabe969d
KangOxford <kang@robots.ox.ac.uk>
2023-03-27 04:59:26 +0800

    date label

--- linear_model/ols_backwise/12.1_ols_mv_15min_test.py
+++ linear_model/ols_backwise/12.1_ols_mv_15min_test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os;from os import listdir;from os.path import isfile, join
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt; plt.rcParams["figure.figsize"] = (12, 8)
+import matplotlib.dates as mdates
 from sklearn.metrics import r2_score
 
 '''transform'''
@@ -22,24 +23,15 @@ else:print("Unknown operating system")
 try: listdir(out_path)
 except:import os;os.mkdir(out_path)
 
-onlyfiles = sorted([f for f in listdir(data_path) if isfile(join(data_path, f))])
-df_lst = pd.concat(map(lambda file: pd.read_pickle(data_path + file), onlyfiles))
-# groupby date
-mean_date = df_lst.groupby('date').mean()
-r_squared_mean_date = r2_score(mean_date['yTrue'], mean_date['yPred'])
-mse_date = df_lst.groupby('date').apply(lambda x: mean_squared_error(x['yTrue'],x['yPred']))
-# groupby date and time
-df_lst['date_time'] = df_lst.date.apply(str) + df_lst.timeHMs.apply(lambda x: str(x).zfill(4))
-mean_date_time = df_lst.groupby('date_time').mean()
-r_squared_mean_date_time = r2_score(mean_date_time['yTrue'], mean_date_time['yPred'])
-mse_date_time = df_lst.groupby('date_time').apply(lambda x: mean_squared_error(x['yTrue'],x['yPred']))
-
-
-
 def plot_pred_true(value,title, size=(12, 8)):
     plt.rcParams["figure.figsize"] = size
-    plt.plot(value.yTrue.values, label='True')
-    plt.plot(value.yPred.values, label='Pred')
+    value['date'] = pd.to_datetime(value.index, format='%Y%m%d')
+    plt.plot(value.date, value.yTrue, label = 'True')
+    plt.plot(value.date, value.yPred, label = 'Pred')
+    # Set the x-axis format to display dates
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    # Rotate the x-axis tick labels to avoid overlap
+    plt.gcf().autofmt_xdate()
     plt.title(title)
     plt.legend();
     fig_name = out_path + title +'.png'
@@ -49,31 +41,64 @@ def plot_pred_true(value,title, size=(12, 8)):
 
 def plot_mse(value, title, size=(12, 8)):
     plt.rcParams["figure.figsize"] = size
-    plt.plot(value.values, label="MSE");
+    value['date'] = pd.to_datetime(value.index, format='%Y%m%d')
+    plt.plot(value.date, value.values, label = 'MSE')
+    # Set the x-axis format to display dates
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    # Rotate the x-axis tick labels to avoid overlap
+    plt.gcf().autofmt_xdate()
     plt.title(title)
     plt.legend();
     plt.savefig(out_path + title + '.png')
     plt.show()
 
-'''plot'''
-'''date'''
-plot_pred_true(mean_date,"yPred_&_yTrue ols15min mean_by_date")
-plot_mse(mse_date,"yPred_&_yTrue ols15min mse_by_date")
-
-'''date_time'''
-plot_pred_true(mean_date_time,"yPred_&_yTrue ols15min mean_by_date_&_time",(40,10))
-plot_mse(mse_date_time,"yPred_&_yTrue ols15min mse_by_date_&_time",(40,10))
 
 
+if __name__ == "__main__":
 
-# '''anomaly'''
-# '''anomaly handling'''
-# mse_date_gpd = df_lst.groupby('date_time')
-# anomaly_sample = mse_date_gpd.get_group('201707251537')
-#
-# plt.plot(anomaly_sample.yTrue.values,label='True');plt.plot(anomaly_sample.yPred.values,label='Pred')
-# plt.xticks(fontsize=20);plt.yticks(fontsize=20);
-# plt.legend(fontsize=20);
-# plt.show()
+    onlyfiles = sorted([f for f in listdir(data_path) if isfile(join(data_path, f))])
+    df_lst = pd.concat(map(lambda file: pd.read_pickle(data_path + file), onlyfiles))
+    # groupby symbol
+    igpd = iter(df_lst.groupby('symbol'))
+    item = next(igpd)[1]
+    def compute_batch_r2(batch):
+        y_true = batch['yTrue']
+        y_pred = batch['yPred']
+        return r2_score(y_true, y_pred)
+    # Apply the compute_batch_r2 function to every batch of 20 rows
+    r2_scores = item.groupby(item.index // 20).apply(compute_batch_r2)
+
+    # groupby date
+    mean_date = df_lst.groupby('date').mean()
+    r_squared_mean_date = r2_score(mean_date['yTrue'], mean_date['yPred'])
+    mse_date = df_lst.groupby('date').apply(lambda x: mean_squared_error(x['yTrue'],x['yPred']))
+    # groupby date and time
+    df_lst['date_time'] = df_lst.date.apply(str) + df_lst.timeHMs.apply(lambda x: str(x).zfill(4))
+    mean_date_time = df_lst.groupby('date_time').mean()
+    r_squared_mean_date_time = r2_score(mean_date_time['yTrue'], mean_date_time['yPred'])
+    mse_date_time = df_lst.groupby('date_time').apply(lambda x: mean_squared_error(x['yTrue'],x['yPred']))
+
+
+
+    '''plot'''
+    '''date'''
+    plot_pred_true(mean_date,"yPred_&_yTrue ols15min mean_by_date")
+    plot_mse(mse_date,"yPred_&_yTrue ols15min mse_by_date")
+
+    '''date_time'''
+    plot_pred_true(mean_date_time,"yPred_&_yTrue ols15min mean_by_date_&_time",(40,10))
+    plot_mse(mse_date_time,"yPred_&_yTrue ols15min mse_by_date_&_time",(40,10))
+
+
+
+    # '''anomaly'''
+    # '''anomaly handling'''
+    # mse_date_gpd = df_lst.groupby('date_time')
+    # anomaly_sample = mse_date_gpd.get_group('201707251537')
+    #
+    # plt.plot(anomaly_sample.yTrue.values,label='True');plt.plot(anomaly_sample.yPred.values,label='Pred')
+    # plt.xticks(fontsize=20);plt.yticks(fontsize=20);
+    # plt.legend(fontsize=20);
+    # plt.show()
 
 
