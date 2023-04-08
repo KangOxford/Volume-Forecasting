a1c3ce90c7e11807af710cd9383aad92c59de421
KangOxford <kang@robots.ox.ac.uk>
2023-04-08 13:22:25 +0100

    update

--- linear_model/ols_backwise/10_11.2.test_ols_15min.py
+++ linear_model/ols_backwise/10_11.2.test_ols_15min.py
@@ -46,14 +46,14 @@ combined_array = np.array(list(zip(array1, array2))).flatten()
 
 
 # regulator = "Ridge"
-regulator = "Lasso"
-# regulator = "OLS"
+# regulator = "Lasso"
+regulator = "OLS"
 
 if __name__=="__main__":
     r2_score_arr_list = []
     mse_score_arr_list = []
     y_true_pred_arr_list = []
-    for i in tqdm(range(len(onlyfiles))): # on mac4
+    for i in [0]: # on mac4
         bin_size = 26
         num_day = 10
         file = onlyfiles[i]
@@ -76,7 +76,7 @@ if __name__=="__main__":
         y_true_pred_list = []
         times = np.arange(0,dflst.shape[0]//bin_size-num_day)
         for time in times:
-            print(f"time: {time}/{times[-1]}")
+            print(f"time: {time}")
             index = bin_size * time
             y_list =[]
             for drift in range(bin_size):
@@ -110,9 +110,8 @@ if __name__=="__main__":
                             columns.remove('const')
                             X = pd.DataFrame(X_test[columns]).T
                             X = sm.add_constant(X, has_constant='add')
-                        y_pred = model.predict(X).values
-                        # assert type(y_pred) == np.float64
-                        return y_pred[0]
+                        y_pred = model.predict(X)
+                        return y_pred
                     elif regulator in ["Lasso","Ridge"]:
                         # print("LASSO / RIDGE")
                         def find_best_regularity_alpha(X_train, y_train):
@@ -135,14 +134,14 @@ if __name__=="__main__":
                         reg.fit(X_train, y_train)
                         X = pd.DataFrame(X_test).T
                         y_pred = reg.predict(X)
-                        return y_pred[0]
+                        return y_pred
                     else: raise NotImplementedError
                 # reg = regularity_ols(X_train, y_train, regulator)
                 # y_pred = reg.predict(X_test)
                 # y_pred = np.array([0]) #$
                 y_pred = regularity_ols(X_train, y_train, X_test, regulator)
                 min_limit, max_limit = y_train.min(), y_train.max()
-                y_pred = min(max(min_limit, y_pred), max_limit)
+                # y_pred = np.vectorize(lambda x: min(max(min_limit, x),max_limit))(y_pred)[0]
                 y_list.append([y_test,y_pred])
             y_arr = np.array(y_list)
             y_test = y_arr[:,0]; y_pred = y_arr[:,1]
@@ -170,51 +169,3 @@ if __name__=="__main__":
         r2_score_arr_list.append(r2_score_arr)
         mse_score_arr_list.append(mse_score_arr)
         y_true_pred_arr_list.append(y_true_pred_arr)
-
-        def plot(r2_score_arr, name):
-            fig_path = path + "04_pred_true_fig/"
-            import matplotlib.pyplot as plt
-            import matplotlib.dates as mdates
-            # x_axis = np.arange(len(values))
-            dates = r2_score_arr[:,-2]
-            values = r2_score_arr[:,-1].astype(np.float32)
-            dates = pd.to_datetime(dates, format='%Y%m%d')
-            x_axis = dates
-            plt.plot(x_axis, values)
-            plt.plot(x_axis, np.full(len(values), values.mean()), label=f'Mean: {values.mean():.2f}')  # mean value
-            # Set the x-axis format to display dates
-            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-            # Rotate the x-axis tick labels to avoid overlap
-            plt.gcf().autofmt_xdate()
-            plt.xlabel('Date')
-            plt.ylabel(name + ' Score')
-            title = name + " Score for Single Stock " + file[:-4]
-            plt.title(title)
-            plt.legend()
-            plt.savefig(fig_path+title)
-            plt.show()
-        # plot(r2_score_arr, "R2")
-        # plot(mse_score_arr, "MSE")
-
-    r2_score_arr_arr = np.array(r2_score_arr_list).reshape(-1,3)
-    mse_score_arr_arr = np.array(mse_score_arr_list).reshape(-1,3)
-    y_true_pred_arr_arr = np.array(y_true_pred_arr_list).reshape(-1,4)
-
-    r2_score_arr_df = pd.DataFrame(r2_score_arr_arr,columns=["symbol",'date','value'])
-    mse_score_arr_df = pd.DataFrame(mse_score_arr_arr,columns=["symbol",'date','value'])
-    y_true_pred_arr_df = pd.DataFrame(y_true_pred_arr_arr,columns=["symbol",'date','true','pred'])
-
-    result_data_path = path + "05_result_data_path/"+regulator+"/"
-    try:listdir(result_data_path)
-    except:import os;os.mkdir(result_data_path)
-
-    r2_score_arr_df.to_csv(result_data_path + "r2_score.csv")
-    mse_score_arr_df.to_csv(result_data_path + "mse_score.csv")
-    y_true_pred_arr_df.to_csv(result_data_path + "y_true_pred.csv")
-
-
-
-    array1 = np.concatenate( [np.arange(1,10,0.01), np.arange(10,50,0.1) ])
-    array2 = np.arange(1,0.001,-0.001)
-    combined_array = np.array(list(zip(array1, array2))).flatten()
-    print(combined_array)
